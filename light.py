75452f1aba73bfc68275867132a9132e54f33114
William Mura <william.mura@nuance.com>
2018-02-23 13:54:05 -0500

    Adding plugin

---
+++ light.py
@@ -0,0 +1,29 @@
+from urllib.request import Request
+from urllib.request import urlopen
+ 
+from errbot import botcmd
+from errbot import BotPlugin
+
+
+class Light(BotPlugin):
+
+    api_url = URL = 'http://10.3.18.205:8888/{0}'
+
+    @botcmd
+    def light(self, msg):
+        """ Enable/Disable Mix Light """
+
+        # Get state
+        state = None
+        if msg == 'on':
+            state = 'activate'
+        elif msg == 'off':
+            state = 'deactivate'
+        else:
+            return "This is not a valid state man, please use 'on' or 'off'"
+
+        # Request server
+        if state:
+            url = api_url.replace(state)
+            with urlopen(Request(url, {})) as response:
+                return response
